a7ac9a70e107b2f2f1c726ebfefb1de09fefa6d0
advistasyam <advis.tasyah@ui.ac.id>
2022-02-12 21:36:45 +0700

    change domain to paytungan.vercel.app

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Paytungan By <a href="https://nextjs.org">Next.js!</a>
+          Paytungan Ubul Ubul By <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
